a081a82ee7a2587258ee6fe8b82c2965d34b32b6
kurjaakoodia <atte.kilpelainen@metropolia.fi>
2023-11-14 14:11:18 +0200

    Initial Commit

---
+++ webdev-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  webdev-project
+//
+//  Created by Atte Kilpeläinen on 14.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ webdev-project/webdev_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  webdev_projectApp.swift
+//  webdev-project
+//
+//  Created by Atte Kilpeläinen on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct webdev_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
